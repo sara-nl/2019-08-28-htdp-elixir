1389f97c6ad467b58635be928a77a48c78bdc8f6
Maithili Kalamkar <maithili86@gmail.com>
2019-07-24 14:39:22 +0200

    Create job-submit-variant-calling-tmpdir.sh

---
+++ _episodes/scripts/job-submit-variant-calling-tmpdir.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -c 1
+#SBATCH --constraint=skylake
+
+mkdir "$TMPDIR"/var-calling
+cd "$TMPDIR"/var-calling
+
+cp $HOME/ecoli-analysis/run-variant-calling-tmpdir.sh .
+
+time bash run-variant-calling-tmpdir.sh 
